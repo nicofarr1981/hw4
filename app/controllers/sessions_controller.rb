6f828c0d873e598971adb8a6336e5ad755e8fcb4
Nico Farr <nicofarr@gmail.com>
2022-05-27 23:23:05 +0000

    solved main issues

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,29 @@
 class SessionsController < ApplicationController
-  def new
-  end
 
-  def create
-  end
+    def new
+    end
 
-  def destroy
-  end
+    def create
+        @user = User.find_by({ "email" => params["email"] })
+        if @user
+            if BCrypt::Password.new(@user["password"]) == params["password"]
+                session["user_id"] = @user["id"]
+                flash["notice"] = "You're in!"
+                redirect_to "/" 
+            else
+                flash["notice"] = "Incorrect credentials!"
+                redirect_to "/login"     
+            end 
+        else
+            flash["notice"] = "Incorrect credentials!"
+            redirect_to "/login"
+        end
+    end
+
+    def destroy
+        flash["notice"] = "You are logged out"
+        session["user_id"] = nil
+        redirect_to "/login"
+    end
+
 end
-  
