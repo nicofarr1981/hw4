938f94bffccb1b5f4e1c5695fa3b77ce135b07ba
Nico Farr <nicofarr@gmail.com>
2022-05-28 00:51:33 +0000

    fine tuning

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,10 +6,14 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ 
-      "place_id" => @place["id"],
-      "user_id" => @current_user["id"]
-    })
+    if @current_user 
+      @posts = Post.where({ 
+        "place_id" => @place["id"],
+        "user_id" => @current_user["id"]
+      })
+    else
+      @posts = nil
+    end
   end
 
   def new
